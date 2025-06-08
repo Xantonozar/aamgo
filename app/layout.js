57e0cf76d35af0f722fb4d4574d8b45fa98c377d
Ozar <87942063+Xantonozar@users.noreply.github.com>
2025-06-08 20:03:26 +0600

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -53,7 +53,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="/vercel.svg" sizes="any" />
       </head>
       <body className={inter.className}>
         {children}
