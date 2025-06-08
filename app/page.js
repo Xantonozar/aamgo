b49008b5dbb7f5a9820d47e5be3cb6806ce7a3c5
Ozar <87942063+Xantonozar@users.noreply.github.com>
2025-06-08 20:21:46 +0600

    Update page.js

--- app/page.js
+++ app/page.js
@@ -13,7 +13,7 @@ export default function Home() {
         <HeroSection />
         <ProductShowcase />
         <Features />
-        // <Testimonials />
+        
       </main>
       <Footer />
     </div>
